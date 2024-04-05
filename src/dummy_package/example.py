a11d13b54919c28616c5b86101619208c67265e2
TSGreen <tim.green@ons.gov.uk>
2024-04-05 16:52:56 +0100

    feat: Add basic dummy python package

---
+++ src/dummy_package/example.py
@@ -0,0 +1,2 @@
+def perform_action():
+    return("Your package was installed and here is a message to confirm that.")
---
+++ src/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dummy_package',
+      version='0.1',
+      description='A holding package for test purposes',
+      url='https://github.com/datasciencecampus/ci-sandbox/src/dummy_package',
+      author='DSC',
+      author_email='datasciencecampus@ons.gov.uk',
+      license='MIT',
+      packages=['dummy_package'],
+      zip_safe=False)
